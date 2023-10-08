3c78a6364279a1d3c617736bcb022558a495162a
Valentyn Kolesnikov <javadev75@gmail.com>
2023-10-08 10:46:51 +0300

    Improved task 1

---
+++ src/test/scala/g0001_0100/s0001_two_sum/SolutionSpec.scala
@@ -0,0 +1,22 @@
+package g0001_0100.s0001_two_sum
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class SolutionSpec extends AnyFlatSpec with Matchers {
+    it should "twoSum" in {
+        Solution.twoSum(Array(2, 7, 11, 15), 9) shouldEqual Array(0, 1)
+    }
+
+    it should "twoSum2" in {
+        Solution.twoSum(Array(3, 2, 4), 6) shouldEqual Array(1, 2)
+    }
+
+    it should "twoSum3" in {
+        Solution.twoSum(Array(3, 3), 6) shouldEqual Array(0, 1)
+    }
+
+    it should "twoSum4" in {
+        Solution.twoSum(Array(3, 3), 7) shouldEqual Array(-1, -1)
+    }
+}
