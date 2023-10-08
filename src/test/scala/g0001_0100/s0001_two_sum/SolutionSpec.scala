ac929a8ab20367be0cb5765e54387e0eb12781d6
Valentyn Kolesnikov <javadev75@gmail.com>
2023-10-08 10:33:49 +0300

    Fixed test

--- src/test/scala/g0001_0100/s0001_two_sum/SolutionSpec.scala
+++ src/test/scala/g0001_0100/s0001_two_sum/SolutionSpec.scala
@@ -3,9 +3,8 @@ package g0001_0100.s0001_two_sum
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class TwoSumSpec extends AnyFlatSpec with Matchers {
-
-  it should "twoSum" in {
-    Solution.twoSum(Array(2, 7, 11, 15), 9) shouldEqual Array(0, 1)
-  }
+class SolutionSpec extends AnyFlatSpec with Matchers {
+    it should "twoSum" in {
+        Solution.twoSum(Array(2, 7, 11, 15), 9).seq shouldEqual Array(0, 1).seq
+    }
 }
