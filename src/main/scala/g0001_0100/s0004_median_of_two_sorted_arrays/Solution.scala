367c5695a2f464ce909b231ce2f0ed7c9e750aee
Valentyn Kolesnikov <javadev75@gmail.com>
2023-10-29 13:57:36 +0200

    Fixed sonar

--- src/main/scala/g0001_0100/s0004_median_of_two_sorted_arrays/Solution.scala
+++ src/main/scala/g0001_0100/s0004_median_of_two_sorted_arrays/Solution.scala
@@ -3,7 +3,7 @@ package g0001_0100.s0004_median_of_two_sorted_arrays
 // #Hard #Top_100_Liked_Questions #Top_Interview_Questions #Array #Binary_Search #Divide_and_Conquer
 // #Big_O_Time_O(log(min(N,M)))_Space_O(1)
 
-@SuppressWarnings({"scala:S3776"})
+@SuppressWarnings(Array("scala:S3776"))
 object Solution {
     def findMedianSortedArrays(nums1: Array[Int], nums2: Array[Int]): Double = {
         if (nums2.length < nums1.length) {
