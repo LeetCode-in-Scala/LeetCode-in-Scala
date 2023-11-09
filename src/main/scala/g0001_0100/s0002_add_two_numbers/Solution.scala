14ab1629023d01c8f03ebb09c2beabe39f2b0ece
Valentyn Kolesnikov <javadev75@gmail.com>
2023-11-09 05:19:31 +0200

    Update Solution.scala

--- src/main/scala/g0001_0100/s0002_add_two_numbers/Solution.scala
+++ src/main/scala/g0001_0100/s0002_add_two_numbers/Solution.scala
@@ -1,11 +1,11 @@
 package g0001_0100.s0002_add_two_numbers
 
-import com_github_leetcode.ListNode
-
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Math #Linked_List #Recursion
 // #Data_Structure_II_Day_10_Linked_List #Programming_Skills_II_Day_15
 // #Big_O_Time_O(max(N,M))_Space_O(max(N,M))
 // #2023_10_17_Time_571_ms_(75.12%)_Space_58.8_MB_(68.16%)
+
+import com_github_leetcode.ListNode
 
 /*
  * Definition for singly-linked list.
